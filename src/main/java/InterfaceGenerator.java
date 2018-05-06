cc09b8d6028070bd7d327cae12d437c38a6cfc2b
MW_01 <marco.wyssmann@students.ffhs.ch>
2018-05-06 17:48:59 +0200

    kommentare

--- src/main/java/InterfaceGenerator.java
+++ src/main/java/InterfaceGenerator.java
@@ -8,13 +8,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * 
- */
 
-/**
- * @author mwyss
- *
+/**Das Programm bekommt einen vollqualifizierten Namen einer Klasse und erzeugt daraus ein Interface
+ * 
+ * @author Stefan Nyffenegger
+ * @author Benjamin Steffen
+ * @author Marco Wyssmann
+ * @version 1.0
+ * 
  */
 public class InterfaceGenerator {
 
@@ -32,6 +33,58 @@ public class InterfaceGenerator {
 
     }
 
+    
+    /**
+     * @param args Ein String Array mit den Uebergabeparametern
+     */
+    public static void main(String[] args) {
+    
+        if (args.length != 1) {
+            System.out.println("Usage: Bitte einen voll-qualifizierten Klassennamen eingeben!>");
+            System.exit(1);
+    
+        }
+    
+        String klassenName = args[0];
+        klassenName.trim();
+        InterfaceGenerator generator = new InterfaceGenerator(klassenName);
+    
+    }
+
+    /**Die Methode generiert im entsprechenden Pfad ein neues Interface-File entsprechend
+     *  der uebergebenen Klasse
+     * 
+     * 
+     * @param klasse Die Klasse aufgrund derer ein Interface generiert werden soll
+     * @param methoden Ein Array mit den Methoden, welche fuer diese Klasse gefunden wurden
+     */
+    public static void generiereInterfaceFile(Class<?> klasse, Method[] methoden) {
+        String klassenName = trimmeKlassenName(klasse);
+        if (!Files.exists(Paths.get(PFAD + klassenName + ".java"))) {
+            try (FileWriter writer = new FileWriter(PFAD + klassenName + ".java")) {
+        	writer.write(InterfaceGenerator.class.getPackageName() + "\n");
+        	writer.write("public interface" + klassenName + "{" + "\n");
+        	for (int i = 0; i < methoden.length; i++) {
+        	    Method methode = methoden[i];
+        	    writer.write(Modifier.toString(methode.getModifiers()) + " " + methode.getReturnType() + " "
+        		    + methode.getName() + erstelleParameterString(methode.getParameterTypes()) + "\n");
+        	}
+        	writer.write("}");
+        	System.out.println("Das Interface mit dem Namen" + klassenName + "wurde erzeugt und befindet sich im "
+        		+ "Ordner lib\\generierte Interfaces\\ ");
+            } catch (IOException e) {
+        	e.printStackTrace();
+            }
+        } else {
+            System.out.println("Das File war bereits vorhanden");
+        }
+    }
+
+    /**Anhand der vollqualifizierten Namens wird die entsprechende Klasse geladen und zurueckgegeben
+     * 
+     * @param klassenName Der vollquealifizierte Name der Klasse
+     * @return die zum Namen gehoerende Klasse
+     */
     public static Class<?> findeKlasse(String klassenName) {
 	Class<?> klasse = null;
 
@@ -44,6 +97,12 @@ public class InterfaceGenerator {
 	return klasse;
     }
 
+    /**Die Methoden der uebergebenen Klasse werden in einem Array zurueckgegeben
+     * Es werden auch die Superklassen untersucht
+     * 
+     * @param klasse Die Klasse deren Methoden ermittelt werden sollen
+     * @return Ein Array mit den gefundenen Methoden
+     */
     public static Method[] findeMethoden(Class<?> klasse) {
 	List<Method> methoden = new ArrayList<>();
 	methoden.addAll(Arrays.asList(klasse.getMethods()));
@@ -56,26 +115,12 @@ public class InterfaceGenerator {
 	return methoden.toArray(new Method[0]);
     }
 
-    public static void generiereInterfaceFile(Class<?> klasse, Method[] methoden) {
-	String klassenName = trimmeKlassenName(klasse);
-	if(!Files.exists(Paths.get(PFAD + klassenName + ".java"))) {
-	    try (FileWriter writer = new FileWriter(PFAD + klassenName + ".java")) {
-		writer.write(InterfaceGenerator.class.getPackageName() + "\n");
-		writer.write("public interface" + klassenName + "{" + "\n");
-        	for (int i = 0; i < methoden.length; i++) {
-        	    Method methode = methoden[i];
-        	    writer.write(Modifier.toString(methode.getModifiers()) + " " + methode.getReturnType() + " "
-        		    + methode.getName() + erstelleParameterString(methode.getParameterTypes())  + "\n");
-        	}
-        	writer.write("}");
-        	System.out.println("Das Interface mit dem Namen" + klassenName + "wurde erzeugt und befindet sich im "
-        		+ "Ordner lib\\generierte Interfaces\\ ");
-	    } catch (IOException e) {
-		e.printStackTrace(); 
-	    }
-	}else { System.out.println("Das File war bereits vorhanden");}
-    }
-
+    /**Die Methode kürzt den vollqualifizierten Namen auf den letzten Teil des 
+     * Namens und fügt "If" an.
+     * 
+     * @param klasse Die Klasse, deren Name gekürzt werden soll
+     * @return der gekürzte Klassenname
+     */
     public static String trimmeKlassenName(Class<?> klasse) {
 	String klassenName = klasse.getName();
 	int cutPosition = klassenName.lastIndexOf(".");
@@ -86,28 +131,15 @@ public class InterfaceGenerator {
 
     }
 
+    /**
+     * @param parameterTypen Ein Array mit den Methoden-Parameter
+     * @return einen String mit den Methoden-Parametern
+     */
     public static String erstelleParameterString(Class<?>[] parameterTypen) {
 	if (parameterTypen.length > 0) {
 	    return "(" + Arrays.toString(parameterTypen) + ")";
 	}
 	return "()";
-    }
-
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-
-	if (args.length != 1) {
-	    System.out.println("Usage: Bitte einen voll-qualifizierten Klassennamen eingeben!>");
-	    System.exit(1);
-
-	}
-
-	String klassenName = args[0];
-	klassenName.trim();
-	InterfaceGenerator generator = new InterfaceGenerator(klassenName);
-
     }
 
 }
