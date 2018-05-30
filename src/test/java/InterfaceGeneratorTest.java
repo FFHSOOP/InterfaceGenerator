80658eac1691e860b974758de6f53b83124fbe2c
MW_01 <marco.wyssmann@students.ffhs.ch>
2018-05-30 20:41:26 +0200

    m

--- src/test/java/InterfaceGeneratorTest.java
+++ src/test/java/InterfaceGeneratorTest.java
@@ -2,12 +2,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * 
- */
+
 
 /**
- * @author mwyss
+ * @author Marco Wyssmann
+ * @author Benjamin Steffen
+ * @author Stefan Nyffenegger
+ * @version 1.0
  *
  */
 public class InterfaceGeneratorTest {
